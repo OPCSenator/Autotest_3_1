d8c550afe82561de200d98ebeeeef297d3fc0bb7
OPCSenator <opc_senator4@mail.ru>
2023-02-18 23:37:41 +0300

    Добавил Аппвеер

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,7 +20,8 @@ public class CallbackTest {
 
     @BeforeAll
     static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver", "drivers/chrome/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "drivers/chrome/chromedriver.exe");
     }
 
     @BeforeEach

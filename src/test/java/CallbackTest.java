c9d6629a124f70769da6eddc3d45587b1aa8839f
OPCSenator <opc_senator4@mail.ru>
2023-02-21 23:08:09 +0300

    Поправил локаторы для имени и телефона и для страницы успеха

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -38,12 +38,11 @@ public class CallbackTest {
       @Test
         void test() {
         driver.get("http://localhost:9999/");
-        List <WebElement> list = driver.findElements(By.className("input__control"));
-        list.get(0).sendKeys("Иван Иванович-Иванов");
-        list.get(1).sendKeys("+01234567890");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+01234567890");
         driver.findElement(By.className("checkbox")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
         }
     }
